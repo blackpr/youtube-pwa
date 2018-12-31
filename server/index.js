3f26df4b5b6a4b2454fd2676ffdeb9978311f870
Tim Pap <blackpr.tim@gmail.com>
2018-12-31 11:38:28 +0200

    extract routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const youtubeRoutes = require('./youtube/youtubeRoutes')
 const consola = require('consola')
 const ytdl = require('ytdl-core')
 const ytsr = require('ytsr')
@@ -24,76 +25,7 @@ async function start() {
     await builder.build()
   }
 
-  app.get('/search', async (req, res) => {
-    let searchTerm = req.query.search_query
-    let nextpageRef = null
-    let sp = req.query.sp
-    if (sp) {
-      nextpageRef = `/results?sp=${sp}&search_query=${searchTerm}`
-    }
-    if (searchTerm) {
-      try {
-        let searchResults = await ytsr(searchTerm, { nextpageRef, limit: 40 })
-        res.json(searchResults)
-      } catch (error) {
-        res.status(500).json({ error: 'ytsr err', error })
-      }
-    } else {
-      res.status(500).json({ error: 'no search term' })
-    }
-  })
-
-  app.get('/video/:videoId', async (req, res) => {
-    let videoId = req.params.videoId
-    if (videoId) {
-      let isValidId = ytdl.validateID(videoId)
-      if (isValidId) {
-        try {
-          let info = await ytdl.getInfo(videoId)
-          let filteredFormats = ytdl.chooseFormat(info.formats, {
-            quality: 'highest',
-            filter: 'audioandvideo'
-          })
-          res.json(filteredFormats)
-        } catch (error) {
-          res.status(500).json({ error: 'ytdl err' })
-        }
-      } else {
-        res.status(500).json({ error: 'invalid video id' })
-      }
-    } else {
-      res.status(500).json({ error: 'no video id' })
-    }
-  })
-
-  app.get('/playlist/:playlistId', async (req, res) => {
-    let playlistId = req.params.playlistId
-    if (playlistId) {
-      try {
-        let playlistResults = await ytpl(playlistId)
-        res.json(playlistResults)
-      } catch (error) {
-        res.status(500).json({ error: 'ytpl error' })
-      }
-    } else {
-      res.status(500).json({ error: 'no playlist id' })
-    }
-  })
-
-  app.get('/channel/:channelId', async (req, res) => {
-    let channelId = req.params.channelId
-    if (channelId) {
-      let channelUrl = `https://www.youtube.com/channel/${channelId}`
-      try {
-        let playlistResults = await ytpl(channelUrl)
-        res.json(playlistResults)
-      } catch (error) {
-        res.status(500).json({ error: 'ytpl error' })
-      }
-    } else {
-      res.status(500).json({ error: 'no channel id' })
-    }
-  })
+  app.use('/getInfo', youtubeRoutes)
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
