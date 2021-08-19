ff5e57d3d79ae2f457f0c3ace37d0e66e0b2a871
Tim Pap <blackpr.tim@gmail.com>
2021-08-19 16:41:29 +0300

    try to find cached video

--- plugins/workbox-videos.js
+++ plugins/workbox-videos.js
@@ -3,7 +3,10 @@ const matchCb = ({ event }) => {
   return url.pathname.includes('googlevideo.com')
 }
 const handlerCb = async ({ event }) => {
-  const cachedResponse = await caches.match(event.request)
+  // const cachedResponse = await caches.match(event.request)
+  // not sure why but this seems to work.
+  // caches.match cant find by the cached file by passing the whole request
+  const cachedResponse = await caches.match(event.request.url)
   console.log({ cachedResponse })
   return cachedResponse || fetch(event.request)
 }
