a7ac419f7058186dde0782a97bb62561af768271
Tim Pap <blackpr.tim@gmail.com>
2021-08-19 14:35:27 +0300

    play with proxys

--- plugins/bg-fetch.js
+++ plugins/bg-fetch.js
@@ -136,8 +136,9 @@ export default async ({ $axios, store }, inject) => {
           thumbnail: getThumbnail(vr),
           thumbnails: _get(vr, 'info.videoDetails.thumbnails', []),
           playlistId: state.playlist.id,
-          url: `https://square-dawn-6a23.blackpr.workers.dev/${encodeURIComponent(
-            _get(vr, 'filteredFormats.url')
+          url: `https://square-dawn-6a23.blackpr.workers.dev/${_get(
+            vr,
+            'filteredFormats.url'
           )}`,
           contentLength: _get(vr, 'filteredFormats.contentLength')
         }
